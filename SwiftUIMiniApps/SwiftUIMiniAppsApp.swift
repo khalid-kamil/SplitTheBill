ab4cb3feee105bda8f40db93593759536ac0e656
Khalid Kamil <khalidkamil28@gmail.com>
2023-11-06 18:12:22 +0000

    Initial Commit

---
+++ SwiftUIMiniApps/SwiftUIMiniAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMiniAppsApp.swift
+//  SwiftUIMiniApps
+//
+//  Created by Khalid Kamil on 06/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMiniAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
